e8ba68a58fc0d86cbdfcaa2f0f4acf805c38ee82
Takuya Arakaki <225arataku@gmail.com>
2026-07-01 00:34:38 +0800

    Install Node for Xcode Cloud build

--- ios/ci_scripts/ci_post_clone.sh
+++ ios/ci_scripts/ci_post_clone.sh
@@ -1,6 +1,15 @@
 #!/bin/sh
 set -e
 
-echo "Running pod install..."
-cd "$(dirname "$0")/.."
+echo "Installing Node.js..."
+brew install node
+
+echo "Node version:"
+node -v
+npm -v
+
+cd ..
+npm install
+
+cd ios
 pod install
